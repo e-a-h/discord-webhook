60cf143d6bc39ef0eb4da47105beb84d1e12c232
AEnterprise <aenterprise2@gmail.com>
2019-08-26 14:56:53 +0200

    fix quotes

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 set -eu
 
-curl -X POST -H "Content-Type: application/json" --data '$DATA' $DISCORD_WEBHOOK
+curl -X POST -H "Content-Type: application/json" --data "$DATA" $DISCORD_WEBHOOK
